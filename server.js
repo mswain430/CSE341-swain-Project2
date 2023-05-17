81aa26aa90db3e8b59687591efea5e9112c44309
Millie Swain <millie.swain@gmail.com>
2023-05-16 17:55:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app
   })
   .use(cors({ methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}))
   .use(cors({ origin: '*' }))
-  //app.use('/', require('./routes').default);  
+  .use('/', require('./routes/index.js'));  
 
  /* process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`)
